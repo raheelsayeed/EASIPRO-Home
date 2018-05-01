2b4265807e9f6dd79151e3fc8e8258992d5ed62c
Raheel Sayeed <raheelsayeed@dhcp-10-17-16-73.tch.harvard.edu>
2018-05-01 15:52:44 -0400

    First Commit

---
+++ EASIPRO-Home/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  EASIPRO-Home
+//
+//  Created by Raheel Sayeed on 5/1/18.
+//  Copyright © 2018 Boston Children's Hospital. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .automatic
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+}
